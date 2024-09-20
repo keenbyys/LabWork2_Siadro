8a735922aa7f7a53dabaa7d106226a47d9dc34d9
keenbyys <166899952+keenbyys@users.noreply.github.com>
2024-09-20 23:44:10 +0300

    1.01

--- LW2_Encapsulation/LW2_Encapsulation/Program.cs
+++ LW2_Encapsulation/LW2_Encapsulation/Program.cs
@@ -12,68 +12,71 @@ namespace LW2_Encapsulation
 {
     class Car
     {
-        private string make;
-        private string model;
-        private int year;
-        private int odometr;
-        private int fuelLevel;
+        // оголошення приватних полів
+        private string make; // марка машини
+        private string model; // модель машини
+        private int year; // рік виготовлення
+        private int odometr; // пробіг
+        private int fuelLevel; // рівень палива (у відсотках)
 
-        public string Make
+        // оголошення публічних властивостей
+        public string Make // властивість марки (читання та запис)
         {
             get { return make; }
             set { make = value; }
         }
 
-        public string Model
+        public string Model // властивість моделі (читання та запис)
         {
             get { return model; }
             set { model = value; }
         }
 
-        public int Year
+        public int Year // властивість року випуску (читання та запис)
         {
             get { return year; }
             set { year = value; }
         }
 
-        public int Odometr
+        public int Odometr // властивість пробігу (тільки запис)
         {
             set { year = value; }
         }
 
-        public int FuelLevel
+        public int FuelLevel // властивість рівню палива (читання та запис із перевіркою, щоб значення було від 0 до 100)
         {
             get { return fuelLevel; }
-            set { if (value >=0 && value <= 100)
+            set { if (value >= 0 && value <= 100)
                     fuelLevel = value; }
         }
 
-        public Car()
+        public Car() // конструктор за замовчуванням
         {
         }
 
-        public Car(string make, string model, int year)
+        public Car(string make, string model, int year) // параметричний конструктор
         {
-            this.make = make;
-            this.model = model;
-            this.year = year;
+            Make = make;
+            Model = model;
+            Year = year;
         }
 
-        public Car(Car car)
+        public Car(Car car) // копійований конструктор
         {
             Make = car.Make;
             Model = car.Model;
             Year = car.Year;
         }
 
-        public void Drive(int kilometers)
+        public void Drive(int kilometers) // публічний метод із параметром, що реалізує рух автомобіля,
+                                          //збільшуючи пробіг та зменшуючи рівень палива (у відсотках)
         {
-            odometr += kilometers;
-            if (kilometers >= 0)
+            odometr += kilometers; // додавання пройдений шлях до пробігу
+            if (kilometers >= 0) // перевірка, щоб шлях був не від'ємним числом
             {
-                for (int i = 1; i <= kilometers/10; i++)
+                for (int i = 1; i <= kilometers/10; i++) // цикл, який буде ділити шлях до тих пір, поки число не буде нуль 
                 {
-                    fuelLevel -= 1;
+                    fuelLevel -= 1; // рівень палива знижуеться на 1% (1 літр) кожні 10 км
                 }
             }
 
@@ -81,7 +84,7 @@ namespace LW2_Encapsulation
                 " Fuel level: {1}%", odometr, fuelLevel);
         }
 
-        public void Refuel(int percentage)
+        public void Refuel(int percentage) // публічний метод
         {
             double checkingFuelLevel;
             checkingFuelLevel = fuelLevel + percentage;
@@ -96,6 +99,11 @@ namespace LW2_Encapsulation
                 Console.WriteLine(" Goes over the limit of the allowable number of liters of fuel.");
             }
 
+        }
+
+        public void Service()
+        {
+            odometr = 0;
         }
 
         public void PrintInfo()
@@ -114,22 +122,51 @@ namespace LW2_Encapsulation
     {
         static void Main(string[] args)
         {
-            int traveledKilometers = 46;
-            int refuelLevel = 20;
+            int traveledKilometers;
+            int refuelLevel;
             int percentageFuelLevel = 100;
 
-            Car car = new Car("Tesla", "Coupe", 1999);
-            car.FuelLevel = percentageFuelLevel;
-            car.PrintInfo();
+            try
+            {
+                Console.WriteLine("\n Enter the make of the machine: " +
+                "Tesla | Toyota | Ford | Honda | BMW");
+                Console.Write(" Make: ");
+                string make = Console.ReadLine();
 
-            Console.WriteLine(" ---------------------------------\n" +
-                " The owner drove {0} kilometers", traveledKilometers);
-            car.Drive(traveledKilometers);
+                Console.WriteLine("\n Enter the model of the machine: " +
+                    "Coupe | Sport Car | Sedan | Station Wagon | Minivan");
+                Console.Write(" Model: ");
+                string model = Console.ReadLine();
 
-            Console.WriteLine(" ---------------------------------\n" +
-                " Trying to refuel: {0}%\n" +
-                " ---------------------------------", refuelLevel);
-            car.Refuel(refuelLevel);
+                Console.WriteLine("\n Enter the year of manufacture of the machine.");
+                Console.Write(" Year: ");
+                int year = Convert.ToInt32(Console.ReadLine());
+
+                Car car = new Car(make, model, year);
+                car.FuelLevel = percentageFuelLevel;
+                car.PrintInfo();
+
+                Console.Write(" ---------------------------------\n" +
+                    " You have driven (km): ");
+                traveledKilometers = Convert.ToInt32(Console.ReadLine());
+
+                car.Drive(traveledKilometers);
+
+                Console.Write(" ---------------------------------\n" +
+                    " Trying to refuel (%): ");
+                refuelLevel = Convert.ToInt32(Console.ReadLine());
+                car.Refuel(refuelLevel);
+
+                Console.Write(" ---------------------------------\n" +
+                    " To be serviced.\n" +
+                    " ---------------------------------\n");
+                car.Service();
+                car.PrintInfo();
+            } 
+            catch
+            {
+                Console.WriteLine("\n Enter data incorrectly! :3");
+            }
 
             Console.ReadLine();
         }
